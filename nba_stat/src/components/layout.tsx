44e25346b25ab257a7451508569b7f3ac9ffdfae
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-03-09 17:19:17 +0900

    first commit carousel

---
+++ nba_stat/src/components/layout.tsx
@@ -0,0 +1,11 @@
+import style from "../styles/layout.module.scss";
+
+const Layout = () => {
+  return (
+    <div className={style.layout}>
+      <span className={style.logo}>NBA</span>
+    </div>
+  );
+};
+
+export default Layout;
