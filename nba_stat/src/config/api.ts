02de575a458a51e5f567266b597367a222540a09
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-07-09 09:54:24 +0900

    fix api key

--- nba_stat/src/config/api.ts
+++ nba_stat/src/config/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: "https://api-nba-v1.p.rapidapi.com",
   headers: {
-    "X-RapidAPI-Key": "83585d71c7mshe9aadef63097536p1e94f6jsn9d6fd2ee9f3a",
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_ENV_API_KEY,
     "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
   },
 });
