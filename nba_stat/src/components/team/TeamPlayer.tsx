67db3f0413dcf6ae05cf686bde04d7083a02a8e6
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-07-25 19:00:50 +0900

    teamSchedule

---
+++ nba_stat/src/components/team/TeamPlayer.tsx
@@ -0,0 +1,15 @@
+import { Box, Paper } from "@mui/material";
+import Title from "../Title";
+
+import teamScheduleData from "@/data/teamSchedule.json";
+
+const TeamPlayer = () => {
+  return (
+    <Box>
+      <Title text="선수" />
+      <Paper className="p-5">qwefqwef</Paper>
+    </Box>
+  );
+};
+
+export default TeamPlayer;
