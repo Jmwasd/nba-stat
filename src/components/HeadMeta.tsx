8ace2357412c07d0e2dda20bf9b9d2db2f96dd0d
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-09-12 21:19:43 +0900

    fix tailwind config

--- src/components/HeadMeta.tsx
+++ src/components/HeadMeta.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 const HeadMeta = () => (
   <Head>
     <title>NBA-STAT</title>
-    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
   </Head>
 );
 
