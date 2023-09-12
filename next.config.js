3f596648975dacb333e8e78f73f6581f5059e314
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-09-12 17:22:05 +0900

    fix next-config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_ENV_API_KEY: process.env.NEXT_PUBLIC_ENV_API_KEY,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
