984ade42ef31e2a5df8787cf360e3b65c9936bb2
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-09-12 20:21:55 +0900

    fix recentMatch

--- src/components/home/recentMatch.tsx
+++ src/components/home/recentMatch.tsx
@@ -41,12 +41,10 @@ const RecentMatch = () => {
     return <Loading height="h-[150px]" />;
   }
 
-  console.log(process.env.NEXT_PUBLIC_ENV_API_KEY);
-
   return (
     <Box className="pb-7">
       <Box className="flex pb-3">
-        <Title text="경기" className="flex items-center relative left-[47.5%] pb-0" />
+        <Title text="경기결과" className="flex items-center relative left-[47.5%] pb-0" />
         <Box className="flex items-center relative bottom-2 ">
           {datePickerValue && (
             <AutorenewIcon
