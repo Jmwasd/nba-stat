9d460201f0f9401ebd62e716fb0d179d1c7573cc
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-09-30 10:58:55 +0900

    setup msw

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 /* eslint-disable react/jsx-props-no-spreading */
 import { Box, Container } from '@mui/material';
 import '../styles/globals.css';
@@ -8,21 +9,27 @@ import HeadMeta from '@/components/HeadMeta';
 import { fetcher } from '@/config/api';
 import { SWRConfig } from 'swr';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <SWRConfig
-    value={{
-      refreshInterval: 0,
-      fetcher: (url: string) => fetcher(url),
-    }}
-  >
-    <HeadMeta />
-    <Container maxWidth={false} sx={{ bgcolor: '#edecec' }}>
-      <Box className="w-[65%] mx-auto my-0 py-10 min-w-[1200px]">
-        <Layout />
-        <Component {...pageProps} />
-      </Box>
-    </Container>
-  </SWRConfig>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+    require('../mocks');
+  }
+
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 0,
+        fetcher: (url: string) => fetcher(url),
+      }}
+    >
+      <HeadMeta />
+      <Container maxWidth={false} sx={{ bgcolor: '#edecec' }}>
+        <Box className="w-[65%] mx-auto my-0 py-10 min-w-[1200px]">
+          <Layout />
+          <Component {...pageProps} />
+        </Box>
+      </Container>
+    </SWRConfig>
+  );
+};
 
 export default App;
