5abcb93154c204499aca3cf16874181dae1c6b0d
Jmwasd <66477332+Jmwasd@users.noreply.github.com>
2023-10-04 20:23:07 +0900

    api route

--- src/consts/api.ts
+++ src/consts/api.ts
@@ -1,11 +1,11 @@
 enum APIv2 {
-  standing = '/standings',
-  player = '/players',
-  playerStats = '/players/statistics',
-  gameStats = '/games/statistics',
-  game = '/games',
-  teamsStats = '/teams/statistics',
-  searchPlayer = '/players',
+  standing = '/api/standings',
+  player = '/api/players',
+  playerStats = '/api/players/statistics',
+  gameStats = '/api/games/statistics',
+  game = '/api/games',
+  teamsStats = '/api/teams/statistics',
+  searchPlayer = '/api/players',
 }
 
 export default APIv2;
